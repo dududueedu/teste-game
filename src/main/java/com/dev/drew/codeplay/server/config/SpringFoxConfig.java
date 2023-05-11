03685acd73369bbc555110b1c8ea56ae54c041e4
Eduardo Gomes <eduardooffside@gmail.com>
2023-05-11 18:21:40 -0300

    add Security-CORS

--- src/main/java/com/dev/drew/codeplay/server/config/SpringFoxConfig.java
+++ src/main/java/com/dev/drew/codeplay/server/config/SpringFoxConfig.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -39,4 +40,11 @@ public class SpringFoxConfig {
 				);
 		return apiInfo;
 	}
+	
+
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:3000")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+    }
 }
